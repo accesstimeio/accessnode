2cb2454ea4830b53a6d47129a303bfab72e3a3f1
Mehmet Rauf Oğuz <88249309+mehmetraufoguz@users.noreply.github.com>
2025-05-06 13:02:07 +0300

    chore(light): update hono

--- packages/light/src/api/index.ts
+++ packages/light/src/api/index.ts
@@ -1,13 +1,14 @@
 import { db } from "ponder:api";
 import schema from "ponder:schema";
 import { Hono } from "hono";
-import { client, graphql } from "ponder";
+import { graphql } from "ponder";
 
 const app = new Hono();
 
-app.use("/sql/*", client({ db, schema }));
-
-app.use("/", graphql({ db, schema }));
+app.use("/api", async (c) => c.json({
+    version: "0.1.0",
+    type: "light"
+}));
 app.use("/graphql", graphql({ db, schema }));
 
 export default app;
